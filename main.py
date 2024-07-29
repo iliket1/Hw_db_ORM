729e9a383acc2e6affe3c032695c967eee7c1103
IliKet <Nika-katena@yandex.ru>
2024-07-29 13:34:12 +1100

    UPD HomeWork

--- main.py
+++ main.py
@@ -12,30 +12,60 @@ create_tables(engine)
 Session = sessionmaker(bind=engine)
 session = Session()
 
-with open('tests_data.json', 'r') as file:
-   data = json.load(file)
+try:
+   with open('tests_data.json', 'r') as file:
+      data = json.load(file)
 
-for record in data:
-   model = {
-      'publisher': Publisher, 
-      'shop': Shop, 
-      'book': Book, 
-      'stock': Stock, 
-      'sale': Sale,
-   }[record.get('model')]
-   session.add(model(id=record.get('pk'), **record.get('fields')))
+   for record in data:
+      model = {
+         'publisher': Publisher, 
+         'shop': Shop, 
+         'book': Book, 
+         'stock': Stock, 
+         'sale': Sale,
+      }[record.get('model')]
+      session.add(model(id=record.get('pk'), **record.get('fields')))
 
-session.commit()
+   session.commit()
 
-pub_name = input()
+except FileNotFoundError:
+   print("Файл 'tests_data.json' не найден.")
 
-result = session.query(Book)\
-   .with_entities(Book.title, Shop.name, Sale.date_sale, Sale.price, Sale.count)\
-   .join(Publisher, Publisher.id == Book.id_publisher)\
-   .join(Stock, Stock.id_book == Book.id)\
-   .join(Shop, Shop.id == Stock.id_shop)\
-   .join(Sale, Sale.id_stock == Stock.id)\
-   .filter(Publisher.name == pub_name).all()
+except json.JSONDecodeError:
+   print("Ошибка декодирования JSON.")
 
-for title, name, date_sale, price, count in result:
-   print(f'Title: {title}, Name: {name}, Date: {date_sale}, Amount: {price * count}')
+# pub_name = input()
+
+# result = session.query(Book)\
+#    .with_entities(Book.title, Shop.name, Sale.date_sale, Sale.price, Sale.count)\
+#    .join(Publisher, Publisher.id == Book.id_publisher)\
+#    .join(Stock, Stock.id_book == Book.id)\
+#    .join(Shop, Shop.id == Stock.id_shop)\
+#    .join(Sale, Sale.id_stock == Stock.id)\
+#    .filter(Publisher.name == pub_name).all()
+
+# for title, name, date_sale, price, count in result:
+#    print(f'Title: {title}, Name: {name}, Date: {date_sale}, Amount: {price * count}')
+
+def get_shops(search):
+
+   result = session.query(Book.title, Shop.name, Sale.date_sale, Sale.price, Sale.count)\
+      .select_from(Shop)\
+      .join(Stock)\
+      .join(Book)\
+      .join(Publisher)\
+      .join(Sale)
+   
+   if search.isdigit():
+      search_pub = result.filter(Publisher.id == search).all()
+   else:
+      search_pub = result.filter(Publisher.name.like(f'%{search}%')).all()
+
+   for title, name, date_sale, price, count in search_pub:
+      print(f"{title: <40} | {name: <10} | {price * count: <8} | {date_sale.strftime('%d-%m-%Y')}")
+
+
+if __name__ == '__main__':
+
+   search = input('Введите имя или ID публициста: ')
+   get_shops(search)
